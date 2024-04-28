082fbb6148a41ea22133fc0a79ab916eb954ede3
ChewwBacca <azizurrohman29@gmail.com>
2024-04-28 20:31:03 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import project from './components/project.vue'
+import background from './components/background.vue'
+</script>
+
+<template>
+  <project  />
+  <background/>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
+
---
+++ src/components/background.vue
@@ -0,0 +1,33 @@
+<template>
+  <head>
+      <meta charset="UTF-8">
+      <link rel="icon" href="/favicon.ico">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+  </head>
+  <body>
+
+  </body>
+ 
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scooped>
+body{
+  background-image: url("src/assets/shop.jpg");
+  background-repeat: no-repeat;
+  background: cover;
+  background-size: cover;
+  margin: 0;
+  display: flex;
+  place-items: center;
+  min-width: auto;
+  min-height: auto;
+  margin-top: 300px;
+}
+</style>
